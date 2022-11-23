9aef8a0b30a46ba4293a3caf546f4e32de7c17ee
sukerberk1 <szymon.l3232@gmail.com>
2022-11-23 21:31:54 +0100

    Add aplications, define file structure, created landing page
    As in summary.

--- MirrorBuddy/MirrorBuddy/settings.py
+++ MirrorBuddy/MirrorBuddy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'spotted',
+    'chatbot',
+    'map',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'MirrorBuddy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates/', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT= os.path.join(BASE_DIR,'/static/')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static/",
+    os.path.join(BASE_DIR, "static/"),
+]
+#Dont know which one makes it work
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- MirrorBuddy/MirrorBuddy/urls.py
+++ MirrorBuddy/MirrorBuddy/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from map.views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('', Home.as_view(), name='home'),
 ]
